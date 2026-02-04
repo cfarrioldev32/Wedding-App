6e5d28ba413598160dc50817b3cb72c15b758cd1
cfarrioldev32 <cristianfarriol28@gmail.com>
2026-02-04 17:48:51 +0100

    ad

--- src/app/components/invitation/invitation-page.component.ts
+++ src/app/components/invitation/invitation-page.component.ts
@@ -19,7 +19,7 @@ export class InvitationPageComponent implements OnInit, AfterViewInit, OnDestroy
 
   readonly CEREMONY_NAME = 'El Olivar';
   readonly CEREMONY_CITY = 'Alcalá de Henares, Madrid';
-  readonly ceremonyAddress = 'Cam. el Olivar, 9, 28806 Alcalá de Henares, Madrid'; // TODO: completar dirección exacta.
+  readonly ceremonyAddress = 'Complejo "El Olivar", 9, 28806 Alcalá de Henares, Madrid'; // TODO: completar dirección exacta.
 
   readonly GOOGLE_MAPS_URL = 'https://www.google.com/maps?um=1&ie=UTF-8&fb=1&gl=es&sa=X&geocode=Kcc9HdaHSUINMVWNKrJE-Z1y&daddr=Cam.+el+Olivar,+9,+28806+Alcal%C3%A1+de+Henares,+Madrid'; // TODO: pegar link exacto de Google Maps.
 
